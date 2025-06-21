f501508bf281b34a0332c68cd807a79e1c8385ad
boubouleuh <adonis@3000fr.com>
2025-06-21 14:29:06 +0200

    removed useless functions

--- ui/modules/apps/BeamMP-Chat/app.js
+++ ui/modules/apps/BeamMP-Chat/app.js
@@ -231,16 +231,6 @@ async function showChat() {
 // -------------------------------------------- CHAT FADING -------------------------------------------- //
 
 // -------------------------------------------- MESSAGE FORMATTING -------------------------------------------- //
-function applyCode(string, codes) {
-	var elem = document.createElement("span");
-	elem.style.fontSize = "initial";
-	string = string.replace(/\x00*/g, "");
-	for (var i = 0, len = codes.length; i < len; i++) {
-		elem.style.cssText += serverStyleMap[codes[i]] + ";";
-	}
-	elem.innerHTML = string;
-	return elem;
-}
 
 function escapeHTML(text) {
     return text.replace(/&/g, "&amp;")
@@ -291,53 +281,6 @@ function formatChatMessage(string) {
     return result;
 }
 
-// function formatRichString(string) {
-// 	let tempAreaElement = document.createElement('div');
-// 	tempAreaElement.setAttribute("id", "TEMPAREA");
-
-// 	var codes = string.match(/\^.{1}/g) || [],
-// 		indexes = [],
-// 		apply = [],
-// 		tmpStr,
-// 		deltaIndex,
-// 		noCode,
-// 		final = document.createDocumentFragment(),
-// 		i;
-
-// 	for (i = 0, len = codes.length; i < len; i++) {
-// 		indexes.push(string.indexOf(codes[i]));
-// 		string = string.replace(codes[i], "\x00\x00");
-// 	}
-
-// 	if (indexes[0] !== 0) {
-// 		final.appendChild(applyCode(string.substring(0, indexes[0]), []));
-// 	}
-
-// 	for (i = 0; i < len; i++) {
-// 		indexDelta = indexes[i + 1] - indexes[i];
-// 		if (indexDelta === 2) {
-// 			while (indexDelta === 2) {
-// 				apply.push(codes[i]);
-// 				i++;
-// 				indexDelta = indexes[i + 1] - indexes[i];
-// 			}
-// 			apply.push(codes[i]);
-// 		} else {
-// 			apply.push(codes[i]);
-// 		}
-// 		if (apply.lastIndexOf("^r") > -1) {
-// 			apply = apply.slice(apply.lastIndexOf("^r") + 1);
-// 		}
-// 		tmpStr = string.substring(indexes[i], indexes[i + 1]);
-// 		final.appendChild(applyCode(tmpStr, apply));
-//   }
-//   tempAreaElement.innerHTML = final;
-//   var innerHTML = [...final.childNodes].map((n) => n.outerHTML).join("\n");
-
-//   tempAreaElement = undefined;
-
-//   return innerHTML;
-// }
 // -------------------------------------------- MESSAGE FORMATTING -------------------------------------------- //
 
 function storeChatMessage(message) {
