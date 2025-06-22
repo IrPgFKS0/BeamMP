612253851768ba819640fb3d73cff69c030b9abe
boubouleuh <adonis@3000fr.com>
2025-06-22 13:26:56 +0200

    Fixed non working emoji in server names for offline servers by updating the base64 functions

--- ui/modModules/multiplayer/multiplayer.js
+++ ui/modModules/multiplayer/multiplayer.js
@@ -1413,19 +1413,28 @@ function saveFav() {
 	// bngApi.engineLua(`MPConfig.setFavorites(\'${JSON.stringify(favorites).replace(/'/g, "\\'")}\')`);
 }
 
+
 function getRecents() {
-	return new Promise(function(resolve, reject) {
-		var tmpRecents = JSON.parse(localStorage.getItem("recents"));
-		recents = tmpRecents || []; // Moved this to here so that we are no longer awaiting it
-		resolve(tmpRecents || []);
-	});
+    return new Promise(function(resolve, reject) {
+        var encoded = localStorage.getItem("recents");
+        var tmpRecents = [];
+        if (encoded) {
+            try {
+                tmpRecents = JSON.parse(Base64.decode(encoded));
+            } catch (e) {
+                tmpRecents = [];
+            }
+        }
+        recents = tmpRecents;
+        resolve(tmpRecents);
+    });
 }
 
 function addRecent(server, isUpdate) { // has to have name, ip, port
 	server.addTime = Date.now();
 	recents.push(server);
 	recents = recents.slice(-1 * 50); //keep the last 50 entries
-	if(!isUpdate) localStorage.setItem("recents", JSON.stringify(recents));
+	if(!isUpdate) localStorage.setItem("recents", Base64.encode(JSON.stringify(recents)));
 }
 
 globalThis.openExternalLink = function(url){
@@ -1619,160 +1628,12 @@ async function isLauncherConnected() {
 
 
 
-/**
-*
-*  Base64 encode / decode
-*  http://www.webtoolkit.info/javascript_base64.html
-*  https://stackoverflow.com/questions/23223718/failed-to-execute-btoa-on-window-the-string-to-be-encoded-contains-characte
-**/
+// Base64 encoding and decoding functions
 var Base64 = {
-
-    // private property
-    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-
-    // public method for encoding
-    , encode: function (input)
-    {
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        input = Base64._utf8_encode(input);
-
-        while (i < input.length)
-        {
-            chr1 = input.charCodeAt(i++);
-            chr2 = input.charCodeAt(i++);
-            chr3 = input.charCodeAt(i++);
-
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-
-            if (isNaN(chr2))
-            {
-                enc3 = enc4 = 64;
-            }
-            else if (isNaN(chr3))
-            {
-                enc4 = 64;
-            }
-
-            output = output +
-                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-        } // Whend 
-
-        return output;
-    } // End Function encode 
-
-
-    // public method for decoding
-    ,decode: function (input)
-    {
-        var output = "";
-        var chr1, chr2, chr3;
-        var enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-        while (i < input.length)
-        {
-            enc1 = this._keyStr.indexOf(input.charAt(i++));
-            enc2 = this._keyStr.indexOf(input.charAt(i++));
-            enc3 = this._keyStr.indexOf(input.charAt(i++));
-            enc4 = this._keyStr.indexOf(input.charAt(i++));
-
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
-
-            output = output + String.fromCharCode(chr1);
-
-            if (enc3 != 64)
-            {
-                output = output + String.fromCharCode(chr2);
-            }
-
-            if (enc4 != 64)
-            {
-                output = output + String.fromCharCode(chr3);
-            }
-
-        } // Whend 
-
-        output = Base64._utf8_decode(output);
-
-        return output;
-    } // End Function decode 
-
-
-    // private method for UTF-8 encoding
-    ,_utf8_encode: function (string)
-    {
-        var utftext = "";
-        string = string.replace(/\r\n/g, "\n");
-
-        for (var n = 0; n < string.length; n++)
-        {
-            var c = string.charCodeAt(n);
-
-            if (c < 128)
-            {
-                utftext += String.fromCharCode(c);
-            }
-            else if ((c > 127) && (c < 2048))
-            {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-            else
-            {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-
-        } // Next n 
-
-        return utftext;
-    } // End Function _utf8_encode 
-
-    // private method for UTF-8 decoding
-    ,_utf8_decode: function (utftext)
-    {
-        var string = "";
-        var i = 0;
-        var c, c1, c2, c3;
-        c = c1 = c2 = 0;
-
-        while (i < utftext.length)
-        {
-            c = utftext.charCodeAt(i);
-
-            if (c < 128)
-            {
-                string += String.fromCharCode(c);
-                i++;
-            }
-            else if ((c > 191) && (c < 224))
-            {
-                c2 = utftext.charCodeAt(i + 1);
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                i += 2;
-            }
-            else
-            {
-                c2 = utftext.charCodeAt(i + 1);
-                c3 = utftext.charCodeAt(i + 2);
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                i += 3;
-            }
-
-        } // Whend 
-
-        return string;
-    } // End Function _utf8_decode 
-
-}
+    encode: function(input) {
+        return btoa(new TextEncoder().encode(input).reduce((data, byte) => data + String.fromCharCode(byte), ""));
+    },
+    decode: function(input) {
+        return new TextDecoder().decode(Uint8Array.from(atob(input), c => c.charCodeAt(0)));
+    }
+};
