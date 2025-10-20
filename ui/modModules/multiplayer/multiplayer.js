ff8b638343778dbcc5373ae2e719046b6b5ced6d
boubouleuh <adonis@3000fr.com>
2025-10-20 18:56:56 +0200

    fix ^p tag for newlines

--- ui/modModules/multiplayer/multiplayer.js
+++ ui/modModules/multiplayer/multiplayer.js
@@ -1311,30 +1311,7 @@ globalThis.serverStyleMap = {
     '^o': 'italic'
 };
 
-var descStyleMap = {
-    '^0': 'color:#000000',
-    '^1': 'color:#0000AA',
-    '^2': 'color:#00AA00',
-    '^3': 'color:#00AAAA',
-    '^4': 'color:#AA0000',
-    '^5': 'color:#AA00AA',
-    '^6': 'color:#FFAA00',
-    '^7': 'color:#AAAAAA',
-    '^8': 'color:#555555',
-    '^9': 'color:#5555FF',
-    '^a': 'color:#55FF55',
-    '^b': 'color:#55FFFF',
-    '^c': 'color:#FF5555',
-    '^d': 'color:#FF55FF',
-    '^e': 'color:#FFFF55',
-    '^f': 'color:#FFFFFF',
-    '^l': 'font-weight:bold',
-    '^m': 'text-decoration:line-through',
-    '^n': 'text-decoration:underline',
-    '^o': 'font-style:italic',
-};
-
-function formatCodes(string) {
+function formatCodes(string, isdesc = false) {
     let result = '';
     let currentText = '';
     let classes = new Set();
@@ -1357,6 +1334,8 @@ function formatCodes(string) {
             flush();
             if (token === '^r') {
                 classes.clear();
+            } else if (isdesc && token === '^p') {
+                currentText += '<br>';
             } else {
                 const cls = globalThis.serverStyleMap?.[token];
                 if (cls?.startsWith('color-')) {
