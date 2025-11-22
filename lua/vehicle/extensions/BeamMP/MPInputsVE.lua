de0b1f0f95c9dfe73b7e35eabcb3894c68534aa9
Olrosse <10835721+Olrosse@users.noreply.github.com>
2025-11-22 01:49:22 +0100

    added automatic exclusion for input electrics
    these are already synced through the input so theres no need to sync the electric as well

--- lua/vehicle/extensions/BeamMP/MPInputsVE.lua
+++ lua/vehicle/extensions/BeamMP/MPInputsVE.lua
@@ -112,11 +112,12 @@ local function getInputs()
 				state = 0
 			end
 			state = math.floor(state * 1000) / 1000
-			if shortName[inputName] then
-				inputName = shortName[inputName]
-			end
 			if lastInputs[inputName] ~= state then
-				inputsToSend[inputName] = state
+				inputsToSend[shortName[inputName] or inputName] = state
+				if not lastInputs[inputName] then
+					MPElectricsVE.excludeElectric(inputName)
+					MPElectricsVE.excludeElectric(inputName.."_input")
+				end
 				lastInputs[inputName] = state
 			end
 		end
@@ -203,6 +204,8 @@ end
 local function onExtensionLoaded()
 	for inputName, state in pairs(input.state) do
 		storeTargetValue(inputName, 0) -- sets all inputs to 0 on spawn so cars don't drive around with the parking brake stuck on
+		MPElectricsVE.excludeElectric(inputName)
+		MPElectricsVE.excludeElectric(inputName.."_input")
 	end
 end
 
